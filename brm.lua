170273d52dce7c9ef768ee8dd61540f10b7230ed
Ross MacLeod <rmm-github@z.odi.ac>
2016-06-16 23:21:58 -0400

    brm

--- brm.lua
+++ brm.lua
@@ -17,31 +17,33 @@ carrotMeasure   = colors.cyan
 local monitor = peripheral.wrap(monitorSide)
 monitor.setTextScale(0.5)
 monitor.clear()
-monitor.write("Bioreactor monitor\n")
+local y = 1
+monitor.setCursorPos(1,y); y = y + 1
+monitor.write("BRM host " .. hostname)
 
 rednet.open(rednetSide)
 rednet.host("brm", hostname)
-
-monitor.write("Rednet open: " .. os.getComputerID() .. "\n")
 
 local bioreactors = {}
 for i = 1,#bioreactorSides do
   local side = bioreactorSides[i]
   local bioreactor = peripheral.wrap(side)
+  monitor.setCursorPos(1,y); y = y + 1
   if bioreactor == nil then
-    monitor.write("Bioreactor " .. side .. " missing!\n")
+    monitor.write("BR " .. side .. " missing!")
   else
-    monitor.write("Bioreactor " .. side .. " found.\n")
+    monitor.write("BR " .. side .. " found.")
   end
   bioreactors[side] = bioreactor
 end
 
 for side,reactor in pairs(bioreactors) do
-  monitor.write("Disabling " .. side .. "\n")
+  monitor.setCursorPos(1,y); y = y + 1
+  monitor.write("Disabling " .. side)
   redstone.setOutput(side, true)
 end
-
-monitor.write("Disabling item feeds\n")
+monitor.setCursorPos(1,y)
+monitor.write("Disabling item feeds")
 redstone.setBundledOutput(bundleSide, 0)
 
 sleep(5)
@@ -72,19 +74,23 @@ function readInventory()
 end
 
 function updateDisplay(inventory, reactants)
+  local y = 1
   monitor.clear()
+  monitor.setCursorPos(1,y); y = y + 1
   monitor.write("BRM " .. hostname .. " (" .. tostring(os.getComputerID) .. ")")
 
-  monitor.write("---------------\n")
+  monitor.setCursorPos(1,y); y = y + 1
+  monitor.write("---------------")
 
   function tern (b)
     if b then return "|" else return " " end
   end
 
+  monitor.setCursorPos(1,y); y = y + 1
   monitor.write("Inv: S " .. tern(inventory.seed1)
                           .. tern(inventory.seed2)
                           .. tern(inventory.seed3) .. " "
              .. "C " .. tern(inventory.carrot) .. " "
-             .. "P " .. tern(inventory.potato) .. "\n")
+             .. "P " .. tern(inventory.potato))
 end
 
